137b02e4a0db4f24d304cccb1973d76737aa18ca
NiuJ1ao <26167136+NiuJ1ao@users.noreply.github.com>
2022-02-08 17:02:24 +0000

    update raft
    Signed-off-by: NiuJ1ao <26167136+NiuJ1ao@users.noreply.github.com>

---
+++ raft/lib/vote.ex
@@ -0,0 +1,25 @@
+
+# distributed algorithms, n.dulay, 8 feb 2022
+# coursework, raft consensus, v2
+
+defmodule Vote do
+
+# s = server process state (c.f. self/this)
+
+# def send_vote_reply_to_candidate(m.candidateP, false) do
+
+# end
+
+# def receive_vote_request_from_candidate(mterm, m) do
+
+# end
+
+# def receive_vote_reply_from_follower(mterm, m) do
+
+# end
+
+# def receive_election_timeout do
+
+# end
+
+end # Vote
