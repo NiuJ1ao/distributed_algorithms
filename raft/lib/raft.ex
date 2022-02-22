b5ede351656ccaf141501c39bbc6b154a2164767
Zion Wang <841048736@qq.com>
2022-02-22 14:19:58 +0000

    Optimize the structure

--- raft/lib/raft.ex
+++ raft/lib/raft.ex
@@ -27,23 +27,38 @@ def start(config,  :cluster_start) do
     Node.spawn(:'server#{num}_#{config.node_suffix}', Database, :start, [config, num])
   end # for
 
-  if config.debug_mode == :TEST_VOTE do
-    # set first server as the leader.
-    serverP   = Enum.at(servers, 0)
-    databaseP = Enum.at(databases, 0)
-    send serverP,   { :TEST_LEADER, servers, databaseP}
-    send databaseP, { :BIND, serverP }
-
-    # others act as follower.
-    for num <- 1..config.n_servers-1 do
-      serverP   = Enum.at(servers, num)
-      databaseP = Enum.at(databases, num)
-      send serverP,   { :TEST_FOLLOWER, servers, databaseP}
+  case config.debug_mode do
+    # Test case 1
+    :TEST_VOTE ->
+       # set first server as the leader.
+      serverP   = Enum.at(servers, 0)
+      databaseP = Enum.at(databases, 0)
+      send serverP,   { :TEST_LEADER, servers, databaseP}
       send databaseP, { :BIND, serverP }
-    end
-  else
 
-    # bind servers and databases
+      # others act as follower.
+      for num <- 1..config.n_servers-1 do
+        serverP   = Enum.at(servers, num)
+        databaseP = Enum.at(databases, num)
+        send serverP,   { :TEST_FOLLOWER, servers, databaseP}
+        send databaseP, { :BIND, serverP }
+      end
+
+    # Test case 2
+    :TEST_CLIENT ->
+      # bind servers and databases
+      for num <- 0 .. config.n_servers-1 do
+        serverP   = Enum.at(servers, num)
+        databaseP = Enum.at(databases, num)
+        send serverP,   { :BIND, servers, databaseP }
+        send databaseP, { :BIND, serverP }
+      end # for
+      # Only create one client
+      Node.spawn(:'client1_#{config.node_suffix}', Client, :start, [config, 1, servers])
+
+    # Normal Run
+    _ ->
+      # bind servers and databases
       for num <- 0 .. config.n_servers-1 do
         serverP   = Enum.at(servers, num)
         databaseP = Enum.at(databases, num)
@@ -51,20 +66,11 @@ def start(config,  :cluster_start) do
         send databaseP, { :BIND, serverP }
       end # for
 
-    if config.debug_mode == :TEST_CLIENT do
-
-      # Only create one client
-      Node.spawn(:'client1_#{config.node_suffix}', Client, :start, [config, 1, servers])
-
-    else
-
       # create 1 client in each client_node and bind to servers
       for num <- 1 .. config.n_clients do
         Node.spawn(:'client#{num}_#{config.node_suffix}', Client, :start, [config, num, servers])
       end # for
-
-    end # debug vote mode
-  end # debug vote mode
+  end
 
 end # start
 
